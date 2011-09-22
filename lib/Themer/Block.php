c7495800eb9218db2e3ad33357476f37eb5c400a
Braden Schaeffer <bschaeffer@me.com>
2011-09-22 16:44:27 -0500

    add a Tumblr block tag parser

---
+++ lib/Themer/Block.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * @package   Themer
+ * @author    Braden Schaeffer
+ * @link      http://github.com/tmbly/themer
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT
+ */
+namespace Themer;
+
+/**
+ * Themer block parser. Renders Tumblr block tags. 
+ */
+class Block {
+
+  /**
+   * Matches all blocks in a given string
+   *
+   * Taken from Thimble by Mark Wunsch -- github.com/mwunsch/thimble
+   *
+   * @access  public
+   * @var     string
+   */
+  const MATCHER = '/{block:([A-Za-z][A-Za-z0-9]*)}(.*?){\/block:\\1}/is';
+
+  /**
+   * Strips the open and close tags from a matched block.
+   *
+   * Taken from Thimble by Mark Wunsch -- github.com/mwunsch/thimble
+   *
+   * @static
+   * @access  public
+   * @param   string  the block to search in
+   * @param   string  the block tag to strip
+   * @return  string  the stripped block
+   */
+  public static function render($block, $tag)
+  {
+    return preg_replace_callback(
+      self::getMatcher($tag),
+      create_function('$matches', 'return $matches[2];'),
+      $block
+    );
+  }
+
+  /**
+   * Renders if blocks
+   *
+   * @static
+   * @access  public
+   * @param   string  the block to search in
+   * @param   string  the block tag name
+   * @param   bool    whether or not we should render the block
+   * @return  string  the stripped block
+   */
+  public static function renderIf($block, $tag, $render = FALSE)
+  {
+    $tag = self::formatIfBlockTag($tag);
+
+    if ($render === TRUE)
+    {
+      $block = self::remove($block, 'IfNot'.$tag);
+      $block = self::render($block, 'If'.$tag);
+    }
+    else
+    {
+      $block = self::remove($block, 'If'.$tag);
+      $block = self::render($block, 'IfNot'.$tag);
+    }
+
+    return $block;
+  }
+
+  /**
+   * Removes all other block tags from a given block
+   *
+   * Taken from Thimble by Mark Wunsch -- github.com/mwunsch/thimble
+   *
+   * @static
+   * @access  public
+   * @param   string  the given block
+   * @return  string  the cleaned up block
+   */
+  public static function cleanup($block)
+  {
+    return preg_replace(self::MATCHER, '', $block);
+  }
+
+  /**
+   * Remove a block completely.
+   *
+   * @static
+   * @access  public
+   * @param   string  the block
+   * @param   string  the block tag to remove
+   * @return  string  the cleaned up block
+   */
+  public static function remove($block, $tag)
+  {
+    foreach (self::find($block, $tag) as $b)
+    {
+      $block = str_replace($b, "", $block);
+    }
+
+    return $block;
+  }
+
+  /**
+   * Attempt to match a set of block open/close tags within a given
+   * block.
+   *
+   * @static
+   * @access  public
+   * @param   string  the parent block
+   * @param   string  the tag to search for
+   * @return  array   empty array for no matches, else the matches
+   */
+  public static function find($block, $tag)
+  {
+    if ( ! preg_match_all(self::getMatcher($tag), $block, $matches))
+    {
+      return array();
+    }
+
+    return $matches[0];
+  }
+
+  /**
+   * Returns a formatted block matcher.
+   *
+   * Taken from Thimble by Mark Wunsch -- github.com/mwunsch/thimble
+   *
+   * @static
+   * @access  public
+   * @param   string  the tag name to use
+   * @return  string  the formated block pattern matcher
+   */
+  public static function getMatcher($tag = '')
+  {
+    return '/{block:('.$tag.')}(.*?){\/block:\\1}/is';
+  }
+
+  /**
+   * Formats a tag name for If and IfNot blocks. We have to reformat the
+   * tag for the blocks (they can't be spaced and the _'s comes from Themer
+   * setting form input names with _'s in the keys)
+   *
+   * @access  public
+   * @param   string  the tag name to format
+   * @return  string  the formatted tag
+   */
+  public static function formatIfBlockTag($tag = '')
+  {
+    return str_replace(array(" ", "_"), "", $tag);
+  }
+}
+
+/* End of file Block.php */
+/* Location: ./Themer/Block.php */
---
+++ test/Themer/Block_Test.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * @package   Themer
+ * @author    Braden Schaeffer
+ * @link      http://github.com/tmbly/themer
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT
+ */
+ 
+use Themer\Block;
+
+class Block_Test extends PHPUnit_Framework_TestCase {
+
+  /**
+   * @test
+   * @covers  Themer\Block::render
+   * @covers  Themer\Block::getMatcher
+   */
+  public function renders_specified_blocks()
+  {
+    $block = "{block:foo}Hello World!{/block:foo}";
+
+    $this->assertEquals(
+      'Hello World!', Block::render($block, 'foo'),
+      'Block::render did not render the tag "{block:foo}" correctly.'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Block::render
+   * @covers  Themer\Block::getMatcher
+   */
+  public function does_not_render_unspecified_blocks()
+  {
+    $block = "{block:foo}Hello World!{/block:foo}";
+
+    $this->assertEquals(
+      $block, Block::render($block, 'bar'),
+      'Block::render rendered the tag "{block:foo}" when it should not have.'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Block::renderIf
+   * @covers  Themer\Block::formatIfBlockTag
+   * @covers  Themer\Block::remove
+   * @covers  Themer\Block::render
+   */
+  public function renders_if_blocks()
+  {
+    $if     = "{block:Iffoo}If{/block:Iffoo}";
+    $ifnot  = "{block:IfNotfoo}IfNot{/block:IfNotfoo}";
+
+    $this->assertEquals(
+      'If', Block::renderIf($if.$ifnot, 'foo', TRUE),
+      'Block::renderIf did not render the "If" block correctly.'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Block::renderIf
+   * @covers  Themer\Block::formatIfBlockTag
+   * @covers  Themer\Block::remove
+   * @covers  Themer\Block::render
+   */
+  public function renders_if_not_blocks()
+  {
+    $if     = "{block:Iffoo}If{/block:Iffoo}";
+    $ifnot  = "{block:IfNotfoo}IfNot{/block:IfNotfoo}";
+
+    $this->assertEquals(
+      'IfNot', Block::renderIf($if.$ifnot, 'foo', FALSE),
+      'Block::renderIf did not render the "IfNot" block correctly.'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Block::cleanup
+   */
+  public function cleanup_removes_all_valid_blocks()
+  {
+    $block = "{block:foo}foo{/block:foo}{block:bar}bar{/block:bar}";
+
+    $this->assertEmpty(
+      Block::cleanup($block),
+      'Block::cleanup did not remove all of the blocks correctly.'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Block::remove
+   * @covers  Themer\Block::find
+   */
+  public function removes_specified_blocks()
+  {
+    $foo = "{block:foo}foo{/block:foo}";
+    $bar = "{block:bar}bar{/block:bar}";
+
+    $this->assertEquals(
+      $foo, Block::remove($foo.$bar, 'bar'),
+      'Block::remove did not remove the tag "{block:bar}" correctly.'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Block::find
+   * @covers  Themer\Block::getMatcher
+   */
+  public function finds_specified_blocks()
+  {
+    $foo = "{block:find}foo{/block:find}";
+    $bar = "{block:find}bar{/block:find}";
+    
+    $results = Block::find($foo.$bar, 'find');
+
+    $this->assertContains(
+      $foo, $results,
+      'Block::find did not find the block "{block:find}foo{/block:find}" correctly.'
+    );
+    
+    $this->assertContains(
+      $bar, $results,
+      'Block::find did not find the block "{block:find}bar{/block:find}" correctly.'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Block::find
+   * @covers  Themer\Block::getMatcher
+   */
+  public function finds_with_no_results_returns_an_array()
+  {
+    $this->assertEquals(
+      array(), Block::find('', 'nonexistent'),
+      'Block::find did not return an empty array() for a non-existent block.'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Block::formatIfBlockTag
+   */
+  public function formats_if_block_tags_correctly()
+  {
+    $tag = "I_ Should HaveNo Spaces Or _'s";
+    $expected = "IShouldHaveNoSpacesOr's";
+
+    $this->assertEquals(
+      $expected, Block::formatIfBlockTag($tag),
+      'Block::formatIfBlockTag did not format the tag correctly.'
+    );
+  }
+}
+
+/* End of file Block_Test.php */
+/* Location: ./test/Themer/Block.php */
