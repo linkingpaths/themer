a6eac8831215e02b7d0825624b5a7e43aa9a1ac2
Braden Schaeffer <hello@manasto.info>
2011-10-03 20:45:09 -0500

    Add blank Data class so we can test the Theme class

---
+++ lib/Themer/Data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package   Themer
+ * @author    Braden Schaeffer
+ * @link      http://github.com/tmbly/themer
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT
+ */
+namespace Themer;
+
+/**
+ * Themer's data handling class.
+ */
+class Data {}
+/* End of file Data.php */
+/* Location: ./Themer/Data.php */
