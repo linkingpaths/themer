8dd1b849534081ed97da5468be47937ff9d56f1f
Braden Schaeffer <hello@manasto.info>
2011-10-07 18:14:41 -0500

    Make Themer\Data implement ArrayAccess

--- lib/Themer/Data.php
+++ lib/Themer/Data.php
@@ -10,6 +10,73 @@ namespace Themer;
 /**
  * Themer's data handling class.
  */
-class Data {}
+class Data {
+
+  /**
+   * @access  protected
+   * @var     array   Theme parsing data
+   */
+  protected $data = array();
+
+  // --------------------------------------------------------------------
+
+  /**
+   * Implemented for the ArrayAccess interface.
+   *
+   * @access  public
+   * @param   string|int  the offset to set
+   * @param   mixed       the value
+   * @return  mixed       the value
+   *
+   * @throws  InvalidArgumentException if the offset is NULL
+   */
+  public function offsetSet($offset, $value)
+  {
+    if (is_null($offset))
+    {
+      throw new \InvalidArgumentException(
+        'Themer does not allow pushing data to the Data class as an array.'
+      );
+    }
+
+    return $this->data[$offset] = $value;
+  }
+
+  /**
+   * Implemented for the ArrayAccess interface.
+   *
+   * @access  public
+   * @param   string|int  the offset to retrieve
+   * @return  mixed       the value of the offset or NULL
+   */
+  public function offsetGet($offset)
+  {
+    return ($this->offsetExists($offset)) ? $this->data[$offset] : NULL;
+  }
+
+  /**
+   * Implemented for the ArrayAccess interface.
+   *
+   * @access  public
+   * @param   string|int  the offset to test
+   * @return  bool        whether or not the offset isset
+   */
+  public function offsetExists($offset)
+  {
+    return isset($this->data[$offset]);
+  }
+
+  /**
+   * Implemented for the ArrayAccess interface.
+   *
+   * @access  public
+   * @param   string|int  the offset to unset
+   * @return  void
+   */
+  public function offsetUnset($offset)
+  {
+    unset($this->data[$offset]);
+  }
+}
 /* End of file Data.php */
 /* Location: ./Themer/Data.php */
