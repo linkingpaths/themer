f3bb1cac94639bb77254cf63a7ea28b400d38941
Braden Schaeffer <hello@manasto.info>
2011-10-08 00:17:40 -0500

    Add an initial Themer\Data class and tests
    This is the beginnings of a data class, not the real thing. I just
    wrote this because I needed something that works.

--- lib/Themer/Data.php
+++ lib/Themer/Data.php
@@ -7,6 +7,8 @@
  */
 namespace Themer;
 
+use Symfony\Component\Yaml\Yaml;
+
 /**
  * Themer's data handling class.
  */
@@ -14,9 +16,129 @@ class Data implements \ArrayAccess {
 
   /**
    * @access  protected
-   * @var     array   Theme parsing data
+   * @var     array   theme parsing data
    */
   protected $data = array();
+
+  /**
+   * @access  protected
+   * @var     array  available load paths to load Themer data from
+   */
+  protected $paths = array();
+
+  /**
+   * @access  protected
+   * @var     array  cache of previously loaded file
+   */
+  protected $loaded = array();
+
+  /**
+   * Constructor.
+   *
+   * @access  public
+   * @return  void
+   */
+  public function __construct()
+  { 
+    $this->addPath(realpath(__DIR__ . DIRECTORY_SEPARATOR . 'data'));
+  }
+
+  /**
+   * Adds a data loading path.
+   *
+   * @access  public
+   * @param   string  a single path to register
+   * @return  void
+   */
+  public function addPath($path)
+  {
+    $path = rtrim($path, DIRECTORY_SEPARATOR);
+
+    if ( ! in_array($path, $this->paths))
+    {
+      $this->paths[] = $path;
+    }
+  }
+
+  /**
+   * Loads a theme data file.
+   *
+   * @access  public
+   * @param   string  the file to load (relative to any Themer\Data::$load_paths)
+   * @return  void
+   */
+  public function load($file)
+  { 
+    $config = array();
+    $found  = FALSE;
+    
+    foreach ($this->paths as $p)
+    {
+      if ( ! ($path = realpath($p . DIRECTORY_SEPARATOR . $file)))
+      {
+        continue;
+      }
+
+      $found = TRUE;
+
+      if ( ! $this->isLoaded($path))
+      {
+        $config = array_merge($config, Yaml::parse($path));
+        $this->loaded[] = $path;
+      }
+    }
+
+    if ( ! $found) 
+    {
+      throw new \InvalidArgumentException("Data file not found: $file");
+    }
+    
+    $this->data = array_merge($this->data, $config);
+  }
+
+  /**
+   * Loads a language file.
+   *
+   * Language files will always be set to the 'lang' index.
+   *
+   * @access  public
+   * @param   string  the language to load (with no '.yml' file extension)
+   * @return  void
+   */
+  public function loadLang($lang)
+  {
+    $path = implode(DIRECTORY_SEPARATOR, array(__DIR__, 'data', 'locales', $lang)) . '.yml';
+
+    if ( ! file_exists($path))
+    {
+      throw new \InvalidArgumentException("Language is not supported: $lang");
+    }
+
+    $this->data['lang'] = Yaml::parse($path);
+  }
+
+  /**
+   * Tests whether a file is loaded or not.
+   *
+   * @access  public
+   * @param   string  the file to test
+   * @return  bool    whether or not the file has been loaded.
+   */
+  public function isLoaded($file)
+  {
+    return (in_array($file, $this->loaded));
+  }
+
+  /**
+   * Returns the currently loaded data.
+   *
+   * @access  public
+   * @return  array   the currently loaded data
+   */
+  public function getData()
+  {
+    return $this->data;
+  }
 
   // --------------------------------------------------------------------
 
@@ -51,7 +173,13 @@ class Data implements \ArrayAccess {
    */
   public function offsetGet($offset)
   {
-    return ($this->offsetExists($offset)) ? $this->data[$offset] : NULL;
+    if ( ! isset($this->data[$offset]))
+    {
+      trigger_error("Undefined index: $offset", E_USER_NOTICE);
+      return NULL;
+    }
+
+    return $this->data[$offset];
   }
 
   /**
---
+++ test/Themer/DataTest.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * @package   Themer
+ * @author    Braden Schaeffer
+ * @link      http://github.com/tmbly/themer
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT
+ */
+namespace Themer;
+
+use Themer\Data;
+use Symfony\Component\Yaml\Yaml;
+
+class DataTest extends \PHPUnit_Framework_TestCase {
+  
+  static public $data_path = '';
+  static public $defaults  = array();
+
+  static public function setUpBeforeClass()
+  {
+    static::$data_path = implode(DIRECTORY_SEPARATOR, array(
+      THEMER_BASEPATH, 'data'
+    ));
+    
+    static::$defaults = Yaml::parse(static::$data_path . DIRECTORY_SEPARATOR . 'defaults.yml');
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Data::__construct
+   * @covers  Themer\Data::addPath
+   */
+  public function adds_correct_default_path_in_constructor()
+  {
+    $data = new Data;
+
+    $this->assertAttributeEquals(
+      array(static::$data_path),
+      'paths',
+      $data
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Data::load
+   * @covers  Themer\Data::getData
+   */
+  public function loads_data()
+  {
+    $data = new Data;
+    $data->load('defaults.yml');
+
+    $this->assertEquals(
+      static::$defaults, $data->getData(),
+      "Themer\Data::__construct did not load the 'defaults.yml' correctly."
+    );
+
+    return $data;
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Data::isLoaded
+   *
+   * @depends loads_data
+   */
+  public function caches_previously_loaded_files($data)
+  { 
+    $this->assertTrue(
+      $data->isLoaded(static::$data_path . DIRECTORY_SEPARATOR . 'defaults.yml')
+    );
+
+    return $data;
+  }
+  
+  /**
+   * @test
+   * @covers  Themer\Data::offsetSet
+   * @covers  Themer\Data::offsetGet
+   * @covers  Themer\Data::offsetExists
+   * @covers  Themer\Data::offsetUnset
+   *
+   * @depends caches_previously_loaded_files
+   */
+  public function is_array_accessible($data)
+  {
+    // Themer\Data::offsetGet
+    $this->assertEquals(
+      static::$defaults['Title'], $data['Title'],
+      "Themer\Data::offsetGet did not return the expected value for 'Title'."
+    );
+
+    // Themer\Data::offsetSet
+    $data['new-index'] = 'Hello World!';
+
+    $this->assertContains(
+      array('new-index' => 'Hello World!'), $data->getData(),
+      "Themer\Data::offsetSet did not set the correct value for 'new-index'."
+    );
+
+    // Themer\Data::offsetUnset
+    // Themer\Data::offsetExists
+    unset($data['new-index']);
+
+    $this->assertFalse(
+      isset($data['new-index']),
+      "Themer\Data::offsetUnset did not unset the index 'new-index' correctly."
+    );
+
+    // Themer\Data::offsetGet
+    //    $value = $data['undefined'];
+    try 
+    {
+      $undefined = $data['undefined'];
+      $this->fail('Themer\Data::offsetGet should trigger an error for undefined offsets.');
+    }
+    catch (\PHPUnit_Framework_Error_Notice $e)
+    {
+      $this->assertEquals(
+        'Undefined index: undefined', $e->getMessage(),
+        'Themer\Data::offsetSet did not throw the correct exception message for undefined offset.'
+      );
+    }
+
+    $this->assertNull(
+      @$data['undefined'],
+      'Themer\Data::offsetGet should return NULL for undefined offsets.'
+    );
+
+    // Themer\Data::offsetSet
+    //    $data[] = 'value';
+    $this->setExpectedException(
+      'InvalidArgumentException',
+      'Themer does not allow pushing data to the Data class as an array.'
+    );
+
+    $data[] = 'value';
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Data::load
+   */
+  public function throws_an_exception_when_loading_non_existent_data_files()
+  {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'Data file not found'
+    );
+
+    $data = new Data;
+    $data->load('some-fake-file.yml');
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Data::loadLang
+   */
+  public function loads_language_files()
+  {
+    $data = new Data();
+    $data->loadLang('en');
+
+    $expected = Yaml::parse(implode(DIRECTORY_SEPARATOR, array(
+      static::$data_path, 'locales', 'en.yml'
+    )));
+
+    $this->assertEquals(
+      $expected, $data['lang'],
+      'Themer\Data::loadLang did not load the expected language.'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Data::loadLang
+   */
+  public function throws_an_exception_when_loading_non_existent_language_files()
+  {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'Language is not supported'
+    );
+
+    $data = new Data;
+    $data->loadLang('some-fake-file');
+  }
+}
+/* End of file DataTest.php */
+/* Location: ./test/Themer/DataTest.php */
