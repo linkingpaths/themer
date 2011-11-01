63135377b566f6f57380643cd68a59b41729b8c5
Braden Schaeffer <hello@manasto.info>
2011-11-01 17:38:41 -0500

    Add a PostParser class

---
+++ lib/Themer/Parser/PostParser.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @package   Themer
+ * @author    Braden Schaeffer
+ * @link      http://github.com/tmbly/themer
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT
+ */
+namespace Themer\Parser;
+
+use Themer\Data;
+use Themer\Parser\BlockParser;
+use Themer\Theme;
+
+/**
+ * Post parser for Tumblr template {block:Posts} blocks.
+ */
+class PostParser extends BaseParser {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function render(Theme $theme, Data $data)
+  {
+    $self = $this;
+
+    $theme->renderTemplate('Posts', function ($template) use ($data, $self)
+    {
+      foreach ($data['Posts'] as $i => $post)
+      {
+        $self->renderPostIndex($template, $i + 1);
+        $self->renderPost($template, $post);
+        $self->renderTags($template, $post['Tags']);
+
+        $template->next();
+      }
+    });
+  }
+
+  /**
+   * Renders a post index blocks like {block:Post1} or {block:Odd}
+   *
+   * @access  public
+   * @param   Themer\Parser\BlockParser   the post block to render
+   * @param   int                         the post index
+   * @return  void
+   */
+  public function renderPostIndex(BlockParser $block, $index)
+  {
+    $block->renderBlock("Post{$index}");
+    $block->renderBlock(($index % 2) ? 'Odd' : 'Even');
+  }
+
+  /**
+   * Renders a single {block:Posts} block using data for a single post.
+   *
+   * @access  public
+   * @return  void
+   */
+  public function renderPost(BlockParser $block, $post)
+  {
+    $block->renderBlock($post['PostType']);
+
+    foreach ($post as $k => $v)
+    {
+      if (is_array($v))
+      {
+        $block->renderEach($k, $v);
+      }
+      elseif ( ! empty($v)) 
+      {
+        $block->renderBlock($k);
+        $block->renderVariable($k, $v);
+      }
+    }
+  }
+
+  /**
+   * Renders single post {block:HasTags} & {block:Tags} blocks.
+   * 
+   * @access  public
+   * @param   Themer\Parser\BlockParser
+   * @param   array   the tag data
+   * @return  void
+   */
+  public function renderTags(BlockParser $block, $tags)
+  {
+    if (empty($tags)) return;
+
+    $data = array();
+
+    foreach ($tags as $tag)
+    { 
+      $tmp['Tag']          = $tag;
+      $tmp['URLSafeTag']   = urlencode($tag);
+      $tmp['TagURL']       = '/tagged/'.str_replace(' ', '+', $tmp['URLSafeTag']);
+      $tmp['TagURLChrono'] = $tmp['TagURL'] . '/chrono';
+
+      array_push($data, array($tmp));
+    }
+
+    $block->renderBlock('HasTags');
+    $block->renderEach('Tags', $data);
+  }
+}
+/* End of file PostParser.php */
+/* Location: ./Themer/Parser/PostParser.php */
---
+++ test/Themer/Parser/PostParserTest.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * @package   Themer
+ * @author    Braden Schaeffer
+ * @link      http://github.com/tmbly/themer
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT
+ */
+namespace Themer\Parser;
+
+use Themer\Data;
+use Themer\Theme;
+
+class PostParserTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * @test
+   * @covers  Themer\Parser\PostParser::render
+   */
+  public function renders_post_blocks()
+  {
+    $block = <<<BLOCK
+{block:Posts}
+This is a post!{/block:Posts}
+BLOCK;
+
+    $expected = <<<BLOCK
+
+This is a post!
+This is a post!
+BLOCK;
+
+    $data = new Data;
+    $post = array('PostType' => '', 'Tags' => array());
+    $data['Posts'] = array($post, $post);
+
+    $theme = new Theme($block, $data);
+    $theme->registerParser(new PostParser);
+
+    $theme->render();
+
+    $this->assertEquals(
+      $expected, $theme->getTheme(),
+      'PostParser::render did not render the {block:Posts} block correctly'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Parser\PostParser::renderPostIndex
+   */
+  public function renders_post_index_blocks()
+  {
+    // Empty data will work fine here, we just need 2 "posts"
+    $content = <<<BLOCK
+{block:Post1}{/block:Post1}
+{block:Post2}{/block:Post2}
+{block:Odd}{/block:Odd}
+{block:Even}{/block:Even}
+BLOCK;
+
+    $block = new BlockParser($content);
+    $post = new PostParser;
+    $post->renderPostIndex($block, 1);
+
+    $this->assertNotContains(
+      "{block:Post1}{/block:Post1}", $block->getBlock(),
+      'PostParser::renderPostIndexes should have rendered {block:Post1}'
+    );
+
+    $this->assertNotContains(
+      "{block:Odd}{/block:Odd}", $block->getBlock(),
+      'PostParser::renderPostIndexes should have rendered {block:Odd}'
+    );
+
+    $this->assertContains(
+      "{block:Post2}{/block:Post2}", $block->getBlock(),
+      'PostParser::renderPostIndexes should not have rendered {block:Post2}'
+    );
+
+    $this->assertContains(
+      "{block:Even}{/block:Even}", $block->getBlock(),
+      'PostParser::renderPostIndexes should not have rendered {block:Even}'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Themer\Parser\PostParser::renderPost
+   */
+  public function renders_single_posts()
+  {
+    $data = array(
+      'PostType' => 'chat',
+      'Lines'    => array(
+        array('Label' => 'Braden', 'Line' => 'Hello, World!'),
+        array('Label' => 'World',  'Line' => 'Hello, Braden!'),
+      ),
+      'Title' => "A quick one while she's away"
+    );
+
+    $content = <<<BLOCK
+{block:Chat}
+{block:Title}{Title}{/block:Title}
+{block:Lines}
+{Label}: {Line}{/block:Lines}
+{/block:Chat}
+BLOCK;
+
+    $expected = <<<BLOCK
+A quick one while she's away
+
+Braden: Hello, World!
+World: Hello, Braden!
+BLOCK;
+
+    $block = new BlockParser($content);
+    $post = new PostParser;
+    $post->renderPost($block, $data);
+
+    $this->assertEquals(
+      $expected, trim($block->getBlock()),
+      "PostParser::renderPost does not seem like its working"
+    );
+  }
+  
+
+  /**
+   * @test
+   * @covers  Themer\Parser\PostParser::renderTags
+   */
+  public function renders_tag_blocks()
+  {
+    $tags = array('jim', 'derrick');
+
+    $content = <<<BLOCK
+{block:Tags}
+Tag:          {Tag}
+URLSafeTag:   {URLSafeTag}
+TagURL:       {TagURL}
+TagURLChrono: {TagURLChrono}
+{/block:Tags}
+BLOCK;
+
+    $expected = <<<BLOCK
+Tag:          jim
+URLSafeTag:   jim
+TagURL:       /tagged/jim
+TagURLChrono: /tagged/jim/chrono
+
+Tag:          derrick
+URLSafeTag:   derrick
+TagURL:       /tagged/derrick
+TagURLChrono: /tagged/derrick/chrono
+BLOCK;
+
+    $block = new BlockParser($content);
+    $parser = new PostParser();
+    $parser->renderTags($block, $tags);
+
+    $this->assertEquals(
+      $expected, trim($block->getBlock()),
+      "PostParser::renderTags did not render the tags as expected"
+    );
+  }
+}
+/* End of file PostParserTest.php */
+/* Location: ./test/Themer/Parser/PostParserTest.php */
