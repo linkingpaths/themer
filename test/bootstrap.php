a5c906eda210aafe7465f81980ff8c299aa583f8
Braden Schaeffer <hello@manasto.info>
2012-02-13 22:13:14 -0600

    Enable easy path template testing
    Templates are TXT files in ./test/_files/templates and are formatted
    like so:
    
    Data       # YAML data representing Themer data
    ----
    Template   # Template to parse
    ----
    Expected   # Expected result of the parse

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,13 +4,28 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 
+// Easy path constant
 if ( ! defined('THEMER_BASEPATH'))
 {
   define('THEMER_BASEPATH', realpath(__DIR__.'/../'));
 }
 
+// Template loading helper function.
+function template($template)
+{
+  $file = THEMER_BASEPATH . '/test/_files/templates/' . $template . '.txt';
+  if (file_exists($file))
+  {
+    $data = explode('----', file_get_contents($file));
+    $data[0] = Symfony\Component\Yaml\Yaml::parse($data[0]);
+    return $data;
+  }
+
+  throw new \Exception("Invalid test template: $template");
+}
+
 // Load up the autoloader.
-require_once THEMER_BASEPATH.'lib/Themer/Utils/Autoloader.php';
+require_once THEMER_BASEPATH.'/lib/Themer/Utils/Autoloader.php';
 Themer\Utils\Autoloader::register();
 
 /* End of file bootstrap.php */
